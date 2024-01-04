393c8c7c785717eed1b9ea141e162aca97f9a745
tzihklearn <1922941549@qq.com>
2024-01-04 20:48:27 +0800

    Init(*):项目初始化

---
+++ src/test/java/io/github/tzih/larkissuebot/LarkIssueBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tzih.larkissuebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LarkIssueBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
